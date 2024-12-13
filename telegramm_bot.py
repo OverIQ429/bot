f3f2b760f612854efd73d50871fc959e347a7331
OverIQ429 <67504104+OverIQ429@users.noreply.github.com>
2024-12-13 12:26:57 +0300

    kek

--- telegramm_bot.py
+++ telegramm_bot.py
@@ -1,6 +1,7 @@
 import random
 import json
 import os
+from random import randint
 
 import numpy as np
 from telebot import types
@@ -9,32 +10,38 @@ import telebot
 class UserPreference():
     def __init__(self, user_id, items, epsilon=0.8):
         self.user_id = user_id
-        self.items = items # Список всех товаров
+        self.items = items # Список всех категорий
         self.epsilon = epsilon
         self.counts = {item_id: 0 for item_id in items}
         self.likes = {item_id: 0 for item_id in items}
+        self.liked_catecorian = []
 
     def choose_item(self):
         if random.random() < self.epsilon:
             item_id = np.random.choice(list(self.items))
-            return item_id, self.items[int(item_id) - 1]  # Возвращаем ID и название товара
+
+            return item_id, self.items[int(item_id)-1]  # Возвращаем ID и название товара
 
         else:
             avg_likes = {item_id: self.likes[item_id] / self.counts[item_id] if self.counts[item_id] > 0 else 0 for item_id in self.items}
             if all(value == 0 for value in avg_likes.values()):
                 item_id = np.random.choice(list(self.items))
-                return item_id, self.items[int(item_id) -1 ]
+                return item_id, self.items[int(item_id)-1]
 
             # Используем Softmax для выбора товара
             probs = np.exp(np.array(list(avg_likes.values())))
             probs /= np.sum(probs)
             item_id = np.random.choice(list(self.items), p=probs)
-            return item_id, self.items[int(item_id) -1]
+            print(item_id)
+            return item_id, self.items[int(item_id)-1]
 
     def update(self, item_id, liked):
         self.counts[item_id[0]] += 1
         if liked == "yes":
             self.likes[item_id[0]] += 1
+            self.liked_catecorian.append(item_id)
+            if len(self.liked_catecorian) == 2:
+                pass
 
     def save(self, filename):
         data = {
@@ -58,7 +65,36 @@ class UserPreference():
 bot = telebot.TeleBot('8117985808:AAEPmdm94_aAXIkQ7EY3A96HtDX1JD6cuyc')
 global user_id
 global filename
-initial_products = {
+initial_products = { 1: {
+    1: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 1", "description": "Описание товара 1"},
+    2: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 2", "description": "Описание товара 2"},
+    3: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 3", "description": "Описание товара 3"},
+    4: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 4", "description": "Описание товара 4"},
+    5: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 5", "description": "Описание товара 5"},
+    6: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 6", "description": "Описание товара 6"},
+    7: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 7", "description": "Описание товара 7"},
+    8: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 8", "description": "Описание товара 8"},
+    9: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 9", "description": "Описание товара 9"},
+    10: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 10", "description": "Описание товара 10"},
+    11: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 11", "description": "Описание товара 11"},
+    12: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 12", "description": "Описание товара 12"},
+
+},
+    2: {
+    1: {"image": "Jimmy.PNG", "name": "Товар 1", "description": "Описание товара 1"},
+    2: {"image": "Jimmy.PNG", "name": "Товар 2", "description": "Описание товара 2"},
+    3: {"image": "Jimmy.PNG", "name": "Товар 3", "description": "Описание товара 3"},
+    4: {"image": "Jimmy.PNG", "name": "Товар 4", "description": "Описание товара 4"},
+    5: {"image": "Jimmy.PNG", "name": "Товар 5", "description": "Описание товара 5"},
+    6: {"image": "Jimmy.PNG", "name": "Товар 6", "description": "Описание товара 6"},
+    7: {"image": "Jimmy.PNG", "name": "Товар 7", "description": "Описание товара 7"},
+    8: {"image": "Jimmy.PNG", "name": "Товар 8", "description": "Описание товара 8"},
+    9: {"image": "Jimmy.PNG", "name": "Товар 9", "description": "Описание товара 9"},
+    10: {"image": "Jimmy.PNG", "name": "Товар 10", "description": "Описание товара 10"},
+    11: {"image": "Jimmy.PNG", "name": "Товар 11", "description": "Описание товара 11"},
+    12: {"image": "Jimmy.PNG", "name": "Товар 12", "description": "Описание товара 12"},
+},
+3: {
     1: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 1", "description": "Описание товара 1"},
     2: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 2", "description": "Описание товара 2"},
     3: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 3", "description": "Описание товара 3"},
@@ -72,6 +108,7 @@ initial_products = {
     11: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 11", "description": "Описание товара 11"},
     12: {"image": "-1wUz1LibUNJjsKBZnAbcIblw1OZO3_130HSybEvqqaHV8TNJS-EYPYO4PQNhacHnFwTG6Csi2ZqWxcXqPD1rqKk.jpg", "name": "Товар 12", "description": "Описание товара 12"},
 }
+}
 
 current_product_index = 0
 
@@ -81,31 +118,36 @@ def handle_start(message):
     global user_pref
     user_id = message.from_user.id
     filename = f"user_preferences_{user_id}.json"
+    rand_int = randint(1, 3)
     global current_product_index
     product_id, product_data = list(initial_products.items())[current_product_index]
     if UserPreference.load(filename):
         user_pref, counts, likes = UserPreference.load(filename)
         user_pref.counts = counts
         user_pref.likes = likes
-        print(product_data)
         print("Данные пользователя загружены.")
     else:
         print(user_id)
         print("Файл с данными пользователя не найден. Создаем новый профиль.")
         user_pref = UserPreference(user_id, initial_products)
-    show_product(message, product_data)
+    print(user_pref)
+    print(product_data)
+    show_product(message, product_data[rand_int])
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback(call):
-    recommended_item = user_pref.choose_item()
-    user_pref.update(recommended_item, call.data)
+    recommended_category = user_pref.choose_item()
+    rand_int = randint(1,12)
+    #recommended_item = recommended_category[1][rand_int]
+    user_pref.update(recommended_category, call.data)
     global current_product_index
-    current_product_index = int(recommended_item[0])
+    current_product_index = rand_int
     user_id = call.from_user.id
     filename = f"user_preferences_{user_id}.json"
     user_pref.save(filename)
-    product_id, product_data = list(initial_products.items())[current_product_index-1]
-    show_product(call.message, product_data)
+    print(list(initial_products.items()))
+    product_id, product_data = list(initial_products.items())[int(recommended_category[0])-1]
+    show_product(call.message, product_data[rand_int])
 
 def show_product(message, product_data):
 
